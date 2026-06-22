ddd3815877ccdc4a01e53a8c1aa70804236a44f6
Lucas Neves <lucasneves4004@gmail.com>
2026-06-22 01:07:07 -0300

    teste

---
+++ src/pages/Clientes.jsx
@@ -0,0 +1,11 @@
+function Clientes() {
+  return (
+    <div>
+      <h1>Gerenciar Clientes</h1>
+      <p>Em breve: formulário de clientes aqui.</p>
+    </div>
+  );
+}
+
+
+export default Clientes;
